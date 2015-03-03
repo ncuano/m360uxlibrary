56afb1d9bd231ccddc75cbf9acb5990c6165c09b
Norman Yuson Cuano <nyc@normanyusoncuano.com>
2015-03-03 17:31:41 -0500

    Initial code set
    Committing initial code set, in case Allen would like to review…

---
+++ javascript/homepage.js
@@ -0,0 +1,65 @@
+$(document)
+  .ready(function() {
+
+    var
+      changeSides = function() {
+        $('.ui.shape')
+          .eq(0)
+            .shape('flip over')
+            .end()
+          .eq(1)
+            .shape('flip over')
+            .end()
+          .eq(2)
+            .shape('flip back')
+            .end()
+          .eq(3)
+            .shape('flip back')
+            .end()
+        ;
+      },
+      validationRules = {
+        firstName: {
+          identifier  : 'email',
+          rules: [
+            {
+              type   : 'empty',
+              prompt : 'Please enter an e-mail'
+            },
+            {
+              type   : 'email',
+              prompt : 'Please enter a valid e-mail'
+            }
+          ]
+        }
+      }
+    ;
+
+    $('.left.sidebar').first()
+      .sidebar('attach events', '.sidebar.toggler')
+    ;
+    
+    $('.sidebar.toggler')
+      .removeClass('disabled')
+    ;
+
+    $('.ui.dropdown')
+      .dropdown({
+        on: 'hover'
+      })
+    ;
+
+    $('.ui.form')
+      .form(validationRules, {
+        on: 'blur'
+      })
+    ;
+
+    $('.masthead .information')
+      .transition('scale in', 1000)
+    ;
+
+    setInterval(changeSides, 3000);
+
+  })
+;
